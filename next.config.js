517354872eb9c23787606f0a15bf8dd620cec3d9
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2022-03-11 09:21:16 +0100

    testing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     trailingSlash: true,
+    swcMinify: true,
+    webpack5: true,
+    poweredByHeader: false,
     i18n: {
         locales: ["de", "en"],
         defaultLocale: "de"
