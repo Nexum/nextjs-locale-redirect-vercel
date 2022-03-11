7a45e4cec5b5ee2f6750529288e53213ca55e228
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2022-03-11 09:34:34 +0100

    testing

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ module.exports = useTranslate(withPWA({
     webpack5: true,
     poweredByHeader: false,
     basePath: "",
+    env: {
+        NEXT_PUBLIC_DOMAIN: "https://nextjs-locale-redirect-vercel.vercel.app",
+    },
     i18n: {
         locales: ["de", "en"],
         defaultLocale: "de"
